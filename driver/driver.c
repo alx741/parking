5eeb9ad1449d4267b91953f389a8e0d117ef2024
Daniel Campoverde <alx@sillybytes.net>
2018-01-10 16:44:02 -0500

    Basic driver

---
+++ driver/driver.c
@@ -0,0 +1,165 @@
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define COMPORT "/dev/ttyUSB0"
+
+// COM PORT file descriptor
+int COM_FD = 0;
+
+
+/* Configure serial port with
+ *
+ * 9600 baud
+ * 8 bit data
+ * No parity
+ * Non blocking reading
+ * Special lines ignore
+ * No control Terminal
+ * Read enable
+ * RAW input/output
+ *
+ * Puts Serial Descriptor in COM_FD
+ * returns 0 if fail, 1 if success
+ */
+int serial_init(void);
+
+/* Returns 1 if hardware is redy to recive a command
+ *
+ * Its a blocking function, returns only when hardware is ready
+ * */
+int wait_hw(void);
+
+typedef enum Axis_ {LEFT, RIGHT} Axis;
+
+int step(Axis axis);
+int rotate(Axis axis, int angle);
+
+
+int main(void)
+{
+    if (geteuid() != 0)
+    {
+        printf("Ejecutar como root: \"sudo driver ...\"");
+        return 1;
+    }
+
+    serial_init();
+    /* step(RIGHT); */
+    rotate(LEFT, 90);
+    return 0;
+}
+
+
+
+int rotate(Axis axis, int angle)
+{
+    int steps = angle / 6; // Each step is ~5 deg
+
+    if (COM_FD < 0)
+    {
+        return 0;
+    }
+    printf("Rotando\n");
+    switch (axis)
+    {
+        case LEFT:
+            while (steps > 0)
+            {
+                step(LEFT);
+                steps--;
+            }
+            break;
+
+        case RIGHT:
+            while (steps > 0)
+            {
+                step(RIGHT);
+                steps--;
+            }
+            break;
+    }
+}
+
+
+int step(Axis axis)
+{
+    if (COM_FD < 0)
+    {
+        return 0;
+    }
+    switch (axis)
+    {
+        case LEFT:
+            write(COM_FD, "l;", 2);
+            wait_hw();
+            break;
+        case RIGHT:
+            write(COM_FD, "r;", 2);
+            wait_hw();
+            break;
+    }
+}
+
+int serial_init(void)
+{
+    if (COM_FD > 0)
+    {
+        return 0;
+    }
+
+    //Open serial port file
+    int fd = open(COMPORT, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd < 0)
+    {
+        COM_FD = -1;
+        return 0;
+    }
+
+    //Configure serial port
+    struct termios config;
+    if (tcgetattr(fd, &config) != 0)
+    {
+        COM_FD = -1;
+        return 0;
+    }
+
+    cfsetispeed(&config, B9600);
+    cfsetospeed(&config, B9600);
+
+    config.c_cflag |= (CLOCAL | CREAD | CS8);
+    config.c_cflag &= ~(PARENB | PARODD);
+    config.c_iflag = 0;
+    config.c_oflag = 0;
+    config.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    config.c_cc[VTIME] = 5;
+    config.c_cc[VMIN] = 0;
+
+    if (tcsetattr(fd, TCSANOW, &config) != 0)
+    {
+        COM_FD = -1;
+        return 0;
+    }
+
+    COM_FD = fd;
+    return 1;
+}
+
+
+int wait_hw(void)
+{
+    char a = 0;
+
+    if (COM_FD < 0)
+    {
+        return 0;
+    }
+
+    while (a != '>')
+    {
+        read(COM_FD, &a, 1);
+    }
+    a = 0;
+    return 1;
+}
