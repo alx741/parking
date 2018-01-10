1c1d4f8e4c285e055497c1df41b5b50b09053d53
Daniel Campoverde <alx@sillybytes.net>
2018-01-10 11:30:47 -0500

    Handle entry arm

--- main.c
+++ main.c
@@ -10,20 +10,75 @@
 #define COMMAND_PROMPT '>'
 #define COMMAND_END ';'
 
+void react_arms(void)
+{
+    // Entry Open
+    if (! (PINB & (1 << PB4)))
+    {
+        // Car on sensor for at least 150ms
+        _delay_ms(150);
+        if (! (PINB & (1 << PB4)))
+        {
+            car_enters(1);
+        }
+    }
+
+    // Entry Close
+    if (PINB & (1 << PB4))
+    {
+        // Car left sensor 300ms ago
+        _delay_ms(300);
+        if (PINB & (1 << PB4))
+        {
+            car_leaves(1);
+        }
+    }
+}
+
+void car_enters(int id)
+{
+    if (id == 1)
+    {
+        servo_open(1);
+    }
+    else
+    {
+        servo_open(2);
+    }
+}
+
+void car_leaves(int id)
+{
+    if (id == 1)
+    {
+        servo_close(1);
+    }
+    else
+    {
+        servo_close(2);
+    }
+}
+
 void init(void)
 {
     stepper_init();
     servo_init();
     uart_init();
+
+    // Sensors inputs
+    /* DDRB |= (1 << PORTB5); */
+    DDRB &= ~(1 << PORTB4);
+    DDRB &= ~(1 << PORTB5);
+    /* PORTB &= ~(1 << PORTB5); */
 }
 
 void execute_command(char *s)
 {
     switch (s[0])
     {
-        case 's':
-            (s[1] == '1') ? servo_toggle(1) : servo_toggle(2);
-            break;
+        /* case 's': */
+        /*     (s[1] == '1') ? servo_toggle(1) : servo_toggle(2); */
+        /*     break; */
 
         case 'x':
             step_right();
@@ -53,19 +108,23 @@ int main(void)
     init();
     putchar(COMMAND_PROMPT);
 
-    while (1) {
-        com_char = getchar();
-        int i = 0;
-        while (com_char != COMMAND_END || i == 10)
-        {
-            command[i] = com_char;
-            com_char = getchar();
-            i++;
-        }
+    while (1)
+    {
+        react_arms();
 
-        execute_command(command);
-        memset(command, 0, com_max);
-        _delay_ms(50);
-        putchar(COMMAND_PROMPT);
+        // code is blocking from here on
+        /* com_char = getchar(); */
+        /* int i = 0; */
+        /* while (com_char != COMMAND_END || i == 10) */
+        /* { */
+        /*     command[i] = com_char; */
+        /*     com_char = getchar(); */
+        /*     i++; */
+        /* } */
+
+        /* execute_command(command); */
+        /* memset(command, 0, com_max); */
+        /* _delay_ms(50); */
+        /* putchar(COMMAND_PROMPT); */
     }
 }
