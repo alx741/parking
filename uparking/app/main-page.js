efc1ac6c682f6ff1008267b7dbdc76818807ef78
Daniel Campoverde <alx@sillybytes.net>
2018-06-27 04:45:58 -0500

    Per spot mark

--- uparking/app/main-page.js
+++ uparking/app/main-page.js
@@ -4,10 +4,29 @@ var observableModule = require("data/observable");
 var page;
 
 var parking = new observableModule.fromObject({
-    b1: 0,
-    b1c: "empty",
-    b2: 0,
-    b2c: "empty"
+    b1s1: "empty",
+    b1s2: "empty",
+    b1s3: "empty",
+    b1s4: "empty",
+    b1s5: "empty",
+    b1s6: "empty",
+    b1s7: "empty",
+    b1s8: "empty",
+    b1s9: "empty",
+    b1s10: "empty",
+    b1s11: "empty",
+    b1s12: "empty",
+    b1s13: "empty",
+    b1s14: "empty",
+    b1s15: "empty",
+
+    b2s1: "empty",
+    b2s2: "empty",
+    b2s3: "empty",
+    b2s4: "empty",
+    b2s5: "empty",
+    b2s6: "empty",
+    b2s7: "empty"
 });
 
 function onNavigatingTo(args) {
@@ -17,52 +36,77 @@ function onNavigatingTo(args) {
 
 function getBlocks(args)
 {
-    var bloque1totales = 0;
-    var bloque2totales = 0;
-    var bloque1vacios = 0;
-    var bloque2vacios = 0;
+    var bloque2 = new Array(15);
+    var bloque2 = new Array(7);
 
-    // fetchModule.fetch("http://192.168.1.50:3000/edificio/1/bloques")
-    fetchModule.fetch("http://104.42.176.241:3000/edificio/1/bloques")
+    let block1spots = 15;
+    let block2spots = 7;
+
+    fetchModule.fetch("http://104.42.176.241:3000/puesto/1")
         .then(handleErrors)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            // console.log(data[0].bloquePuestosTotales);
-            let bloque1totales = data[0].bloquePuestosTotales;
-            let bloque2totales = data[1].bloquePuestosTotales;
+            console.log(data[0].puestoOcupado);
 
-            let bloque1vacios = data[0].bloquePuestosVacios;
-            let bloque2vacios = data[1].bloquePuestosVacios;
+            for (i = 0; i < block1spots; i++)
+            {
+                setState(1, i+1, data[i].puestoOcupado);
+            }
 
-            parking.set("b1", bloque1vacios);
-            parking.set("b2", bloque2vacios);
+            return;
+        });
 
-            parking.set("b1c", getStateClass(bloque1totales, bloque1vacios));
-            parking.set("b2c", getStateClass(bloque2totales, bloque2vacios));
+    fetchModule.fetch("http://104.42.176.241:3000/puesto/2")
+        .then(handleErrors)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data[0].puestoOcupado);
+
+            for (i = 0; i < block2spots; i++)
+            {
+                setState(2, i+1, data[i].puestoOcupado);
+            }
 
             return;
         });
 
 }
 
-function getStateClass(totales, vacios)
+function setState(block, spot, state)
 {
-    var ratio = vacios/totales
-    if (ratio >= 0.3)
+    state_render = ""
+    if (state)
     {
-        return "empty";
-    }
-    else if (ratio >= 0.1)
-    {
-        return "medium";
+        state_render = "full";
     }
     else
     {
-        return "full";
+        state_render = "empty";
     }
+
+    parking.set("b" + block + "s" + spot, "spot " + state_render);
 }
+
+// function getStateClass(totales, vacios)
+// {
+//     var ratio = vacios/totales
+//     if (ratio >= 0.3)
+//     {
+//         return "empty";
+//     }
+//     else if (ratio >= 0.1)
+//     {
+//         return "medium";
+//     }
+//     else
+//     {
+//         return "full";
+//     }
+// }
 
 function handleErrors(response) {
     if (!response.ok) {
